5014c3ea94956c69f3c9ed40a2db477a2e02ec83
Kita-m17 <martinkita04@gmail.com>
2026-08-03 16:10:46 +0200

    added rankName to the rankDTO

--- src/main/java/com/pdr/dtos/RankDTO.java
+++ src/main/java/com/pdr/dtos/RankDTO.java
@@ -5,7 +5,7 @@ import java.util.List;
 //Data Transfer Object (DTO) for the rank trace
 public class RankDTO {
     private int rankNumber;// the rank number
-
+    private String rankName; //the name of the rank (e.g., "R0", "R1", etc.)
     private List<String> knowledgeBase; //the formulas from the knowledge base that belong to this rank
 
     /**
@@ -16,10 +16,15 @@ public class RankDTO {
     /**
      * Parameterised Constructor
      * @param rankNumber
-     * @param kb
+     * @param knowledgeBase
      */
     public RankDTO(int rankNumber, List<String> knowledgeBase){
         this.rankNumber = rankNumber;
+        if (rankNumber == Integer.MAX_VALUE) {
+            this.rankName = "∞"; // Representing the infinite rank
+        } else {
+            this.rankName = "" + rankNumber; // Representing finite ranks as R0, R1, R2, ...
+        }
         this.knowledgeBase = knowledgeBase;
     }
     
@@ -35,6 +40,20 @@ public class RankDTO {
      */
     public void setRankNumber(int rankNumber){
         this.rankNumber = rankNumber;
+    }
+
+    /**
+     * @return String rank name
+     */
+    public String getRankName(){
+        return this.rankName;
+    }
+
+    /**
+     * @param rankName
+     */
+    public void setRankName(String rankName){
+        this.rankName = rankName;
     }
 
     /**
