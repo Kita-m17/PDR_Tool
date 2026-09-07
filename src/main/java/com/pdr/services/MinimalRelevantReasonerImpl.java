614e0a55ed44337650d3db9a6cc0c4f445a4cf48
LiamDeSaldanha <129397980+LiamDeSaldanha@users.noreply.github.com>
2026-09-07 23:08:37 +0200

    removed print statements
    removed print statements

--- src/main/java/com/pdr/services/MinimalRelevantReasonerImpl.java
+++ src/main/java/com/pdr/services/MinimalRelevantReasonerImpl.java
@@ -75,6 +75,9 @@ public class MinimalRelevantReasonerImpl implements ReasonerService {
                 smallestJustificationSize = justification.size();
             }
         }
+        if(!entailment){
+            smallestJustification = new KnowledgeBase();
+        }
         long endTime = System.nanoTime();
         long durationNs = endTime - startTime;
 
