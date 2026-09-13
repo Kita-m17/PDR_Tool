a43d893064cb48ad98276ffa647cad91c7791855
LiamDeSaldanha <129397980+LiamDeSaldanha@users.noreply.github.com>
2026-09-13 18:56:51 +0200

    added classicalJustifications as method to compute for Partition page

--- src/main/java/com/pdr/services/ClassicalJustificationService.java
+++ src/main/java/com/pdr/services/ClassicalJustificationService.java
@@ -1,15 +1,12 @@
 package com.pdr.services;
 
-import com.pdr.models.HittingSetTree;
-import com.pdr.models.KnowledgeBase;
-import com.pdr.models.Node;
+import com.pdr.models.*;
 import com.pdr.utils.Utils;
+import org.springframework.stereotype.Service;
 import org.tweetyproject.logics.pl.reasoner.SatReasoner;
 import org.tweetyproject.logics.pl.sat.Sat4jSolver;
 import org.tweetyproject.logics.pl.sat.SatSolver;
-import org.tweetyproject.logics.pl.syntax.PlBeliefSet;
-import org.tweetyproject.logics.pl.syntax.PlFormula;
-import org.tweetyproject.logics.pl.syntax.Proposition;
+import org.tweetyproject.logics.pl.syntax.*;
 
 import java.util.*;
 
@@ -17,8 +14,17 @@ import java.util.*;
  *
  * @author stevewang
  */
-public class ClassicalJustificationService
+@Service
+public class ClassicalJustificationService implements PartitionService
 {
+
+    private Partition partition;
+    private final KnowledgeBaseService knowledgeBaseService;
+
+    public ClassicalJustificationService(KnowledgeBaseService knowledgeBaseService) {
+        this.knowledgeBaseService = knowledgeBaseService;
+    }
+
     public static List<KnowledgeBase> computeJustification(PlBeliefSet knowledgeBase, PlFormula query)
     {
         SatSolver.setDefaultSolver(new Sat4jSolver());
@@ -65,10 +71,11 @@ public class ClassicalJustificationService
             just.addAll(justification);
             allJustifications.add(just);
 
-
         }
         return allJustifications;
     }
+
+
     
     private static List<PlFormula> computeSingleJustification(PlBeliefSet knowledgeBase, PlFormula query, SatReasoner reasoner)
     {  
@@ -197,5 +204,148 @@ public class ClassicalJustificationService
         
         return result;
     }
-    
+
+    @Override
+    public Partition getPartition(KnowledgeBase knowledgeBase, PlFormula query, boolean isMinimalRelevantClosure) {
+        query = new Negation(((Implication) query).getFirstFormula());
+        long startTime = System.nanoTime();
+
+        SatSolver.setDefaultSolver(new Sat4jSolver());
+        SatReasoner reasoner = new SatReasoner();
+
+        // Construct root node
+        List<PlFormula> rootJustification = computeSingleJustification(knowledgeBase, query, reasoner);
+        Node rootNode = new Node(knowledgeBase, rootJustification);
+
+        // Create a queue to keep track of nodes
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(rootNode);
+        HittingSetTree tree = new HittingSetTree(rootNode);
+        List<PartitionStep> traceSteps = new ArrayList<>();
+        int count =1;
+
+        while(!queue.isEmpty())
+        {
+            Node node = queue.poll();
+
+            for( PlFormula formula : node.getJustification())
+            {
+                PlBeliefSet childKnowledgeBase = Utils.remove(node.getKnowledgeBase(), formula);
+                List<PlFormula> childJustification = computeSingleJustification(childKnowledgeBase, query, reasoner);
+                Node childNode = new Node(childKnowledgeBase, childJustification);
+
+                node.addChildNode(formula, childNode);
+                tree.addNode(childNode);
+
+                if (childJustification != null || childJustification.isEmpty())
+                {
+                    traceSteps.add(PartitionStep.builder()
+                            .withId(count)
+                            .withIsEntailed(true)
+                            .withIsMinimal(false)
+                            .withJustificationsSoFar(new ArrayList<>())
+                            .withMinimalSet(new KnowledgeBase())
+                            .withReason("")
+                            .withSet(new KnowledgeBase(childKnowledgeBase) )
+                            .build()
+
+                    );
+                    count++;
+
+
+                    queue.add(childNode);
+                }
+            }
+        }
+
+        //System.out.println("Tree:");
+        //System.out.println(rootNode.toString());
+
+        List<List<PlFormula>> justifications = rootNode.getAllJustifications();
+
+        List<KnowledgeBase> justificationSoFar = new ArrayList<>();
+        List<KnowledgeBase> allJustifications = new ArrayList<>();
+        KnowledgeBase relevantString = new KnowledgeBase();
+        BaseRank baseRank = knowledgeBaseService.getBaseRank();
+
+        for (List<PlFormula> justification : justifications) {
+            KnowledgeBase just = new KnowledgeBase();
+            KnowledgeBase minimalJustificationStatement = new KnowledgeBase();
+            if (isMinimalRelevantClosure){
+                int lowestRank =Integer.MAX_VALUE;
+                for(Rank rank : baseRank.getRanking()){
+                    for(PlFormula pl : justification){
+
+                        if(rank.getFormulas().contains(pl) && rank.getRankNumber()<lowestRank){
+
+                            lowestRank = rank.getRankNumber();
+                            minimalJustificationStatement.add(pl);
+                        }
+                    }
+
+                }
+                just.addAll(minimalJustificationStatement);
+
+            }else{
+                just.addAll(justification);
+
+            }
+
+
+            if(!allJustifications.contains(just)){
+
+                    justificationSoFar.add(just);
+
+                traceSteps.add(PartitionStep.builder()
+                        .withId(count)
+                        .withIsEntailed(true)
+                        .withIsMinimal(true)
+                        .withJustificationsSoFar(new ArrayList<>(justificationSoFar))
+                        .withMinimalSet(just)
+                        .withReason("")
+                        .withSet(new KnowledgeBase( just))
+                        .build()
+
+                );
+                count++;
+            }
+            allJustifications.add(just);
+            relevantString = relevantString.union(just);
+
+
+        }
+
+        KnowledgeBase irrelevantString = new KnowledgeBase(knowledgeBase);
+
+        KnowledgeBase classicalKnowledgeBase = knowledgeBase.separate()[1];
+
+
+        relevantString = relevantString.difference(classicalKnowledgeBase);
+        irrelevantString = irrelevantString.difference(relevantString);
+        long endTime = System.nanoTime();
+        long durationNs = endTime - startTime;
+
+        double durationSeconds = (double) durationNs / 1_000_000_000.0;
+
+
+        String formattedTime = String.format("%.3fs", durationSeconds);
+
+        this.partition = Partition.builder()
+                .withIrrelevantPartition(irrelevantString)
+                .withRelevantPartition(relevantString)
+                .withTraceSteps(traceSteps)
+                .withExecutionTime(durationSeconds)
+
+
+                .withClassicalStatements(classicalKnowledgeBase)
+                .withKnowledgeBase(knowledgeBase)
+                .build();
+
+        return this.partition;
+    }
+
+    @Override
+    public Partition getInstance() {
+        return this.partition;
+    }
 }
